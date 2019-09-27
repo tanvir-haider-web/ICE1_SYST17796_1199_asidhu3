a76e1b8866e891b386b9bd2443494fa7b663e039
tim <tstanek117@gmail.com>
2019-09-26 20:47:17 -0400

    new Repo for Student Code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.*;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -15,17 +17,41 @@ public class CardTrick {
     
     public static void main(String[] args)
     {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        
+        int numLow = 1;
+        int numHigh = 13;
+        int cardLow = 0;
+        int cardHigh = 3;
+        
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(rand.nextInt(numHigh-numLow) + numLow);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[rand.nextInt(cardHigh-cardLow)]);
+            
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        int userNum = 0;
+        int userSuit = 0;
+        
+              
+        System.out.println("please enter card value(1-13)");
+        userNum = sc.nextInt();
+        System.out.println("Please enter card suit");
+        userSuit = sc.nextInt();
+        
         // and search magicHand here
+        for (int i=0; i<magicHand.length; i++){
+            if (Card[i].cardNum == userNum)
+        }
+        
         //Then report the result here
     }
     
