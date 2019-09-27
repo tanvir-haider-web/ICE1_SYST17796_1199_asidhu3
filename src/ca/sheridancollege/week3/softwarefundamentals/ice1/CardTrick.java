cec89c45fd76fd1748110e1258dd60e67676ad59
justinoh711 <55562876+justinoh711@users.noreply.github.com>
2019-09-26 20:57:04 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -23,6 +23,10 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        Card LuckyCard = new Card;
+        LuckyCard.setvalue(1);
+        LuckyCard.setSuit(3);
+        //Hard coded lucky card
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
