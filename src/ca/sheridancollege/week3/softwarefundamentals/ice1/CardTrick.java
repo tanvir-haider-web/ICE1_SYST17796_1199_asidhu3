9ad57e42f1c4ff2f54a9478312509054e5d0af06
Puneet <Puneet@T450S>
2019-09-26 21:03:31 -0400

    “Finished adding code for 3 and 4 to CardTrick.java”.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Random;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -17,11 +14,20 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        Random rand = new Random();
+        
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            
             //c.setValue(insert call to random number generator here)
+           c.setValue(i); 
+           rand.nextInt(  0 + (int)(Math.random() *13));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+           
+            rand.nextInt(0 + (int)(Math.random()*3));
+            
+            
         }
         
         //insert code to ask the user for Card value and suit, create their card
