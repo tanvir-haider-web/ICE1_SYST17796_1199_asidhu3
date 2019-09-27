436aa02c1f5a49e69d7d00365ab878a6c00b0ff9
tanvir100 <55264016+tanvir100@users.noreply.github.com>
2019-09-26 21:06:18 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -21,6 +21,7 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            luckeycard123
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
