a3771065179d2391290799128d10ec6a43fc1c3d
Sanjeev Kumar <55563005+sanjuchoudhary@users.noreply.github.com>
2019-09-26 21:00:02 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ "hard-coded lucky card"
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
