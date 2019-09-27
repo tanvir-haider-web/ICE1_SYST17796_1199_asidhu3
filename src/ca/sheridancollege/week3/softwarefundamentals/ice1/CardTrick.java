c6b54eab250ff876897306ce23d701df59b7cbe8
NickGjokaj <gjokaj@sheridancollege.ca>
2019-09-26 20:49:09 -0400

    finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import static java.lang.Math.random;
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +22,36 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+             Random r = new Random();
+            
             //c.setValue(insert call to random number generator here)
+            c.setValue(r.nextInt());
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[r.nextInt(3)]);
+            magicHand [i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter card value");
+        int cardVal = input.nextInt();
+        System.out.println("please enter suit");
+        String suitVal = input.next();
+        
         // and search magicHand here
-        //Then report the result here
-    }
+        for (Card magicHand1 : magicHand) {
+            if (magicHand1.getValue() == cardVal) {
+                System.out.println("the card value matches!");
+                
+            } else if (magicHand1.getSuit().equals(suitVal)) {
+                System.out.println("the suit type matches!");
+            } 
+            
+            else{
+                System.out.println("no matches try again");
+            }
+            //Then report the result here
+        }
     
+}
 }
