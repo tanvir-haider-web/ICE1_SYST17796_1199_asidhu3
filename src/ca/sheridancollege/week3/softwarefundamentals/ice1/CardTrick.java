fd3f850d8b642d04b6ef226805763aa9b320dc1f
Main <Main@T420>
2019-09-26 20:57:11 -0400

    First Comments

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -20,8 +20,11 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            
+            
+            // c.setValue(insert call to random number generator here)
+            // c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
         //insert code to ask the user for Card value and suit, create their card
