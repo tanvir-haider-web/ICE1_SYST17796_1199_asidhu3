a735298d4b18016cd38fa262d22f46073af54c87
SarahRadulovich-school <radulovs@sheridancollege.ca>
2019-09-26 20:27:12 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,12 +4,14 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.*;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Sarah Radulovich
  */
 public class CardTrick {
     
@@ -20,13 +22,42 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            Random ran = new Random();
+            c.setValue(ran.nextInt(13)+1);
+            c.setSuit(Card.SUITS[ran.nextInt(4)]);
+            System.out.println("test: "+c.getValue());
+            System.out.println("test: "+c.getSuit());
+            
+            magicHand[i] = c;
         }
-        
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Pick a card value (1-13)");
+        Scanner scan = new Scanner(System.in);
+        int input_value = scan.nextInt();
+        System.out.println("Pick a card suit!");
+        String input_suit = scan.next();
+        
+        boolean result = false;
         // and search magicHand here
+        for (int j=0; j<magicHand.length; j++)
+        {
+            Card c = new Card();
+            if(input_suit.equalsIgnoreCase(magicHand[j].getSuit()))
+            {
+                if(input_value == magicHand[j].getValue())
+                {
+                    result = true;
+                }
+            }
+        }
         //Then report the result here
+        if(result)
+        {
+            System.out.println("Congrats! Your card is in the magic hand!");
+        }
+        else {
+            System.out.println("Your card isn't in the magic hand. Try again next time.");
+        }
     }
     
 }
