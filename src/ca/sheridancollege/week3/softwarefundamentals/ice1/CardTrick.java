37d814f80cc76bc1d9f6061a37a7445fb4869ffc
tim <tstanek117@gmail.com>
2019-09-26 20:49:41 -0400

    new Repo for Student Code

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -3,6 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
+
+/* Timothy Stanek
+* Student Number : 991428353
+*/
+
+
+
+
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 import java.util.*;
